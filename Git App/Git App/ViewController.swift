f0004d6ef169ebcf7ffb0bc8302370ba78bc36a2
Sundaram <hasundaram@usblrhasundarmb.us.deloitte.com>
2018-12-18 09:21:41 +0530

    Initial Commit

---
+++ Git App/Git App/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Git App
+//
+//  Created by Sundaram, Hariharan (US - Bengaluru) on 18/12/18.
+//  Copyright © 2018 Sundaram, Hariharan (US - Bengaluru). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("View Loaded")
+    }
+    
+
+
+}
+
